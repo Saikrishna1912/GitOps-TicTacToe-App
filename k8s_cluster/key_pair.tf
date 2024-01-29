c40d2fd052235afc219fc24c3a9ac3f48aba150a
Sorav Kumar Sharma <add.sorav@gmail.com>
2024-01-29 20:34:14 +0530

    k8s_cluster moved to another repo

--- k8s_cluster/key_pair.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "k8s" {
-  key_name   = "k8s"
-  public_key = file("./ssh_key/k8s.pub")
-}
--- k8s_cluster/output.tf
+++
@@ -1,7 +0,0 @@
-output "master" {
-  value = aws_instance.k8s_master.public_ip
-}
-
-output "workers" {
-  value = aws_instance.k8s_worker[*].public_ip
-}
